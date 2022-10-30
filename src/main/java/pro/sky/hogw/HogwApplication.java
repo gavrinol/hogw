f05b642a3272db1f7e571f956cd997edd7595785
gavrinol <102408582+gavrinol@users.noreply.github.com>
2022-10-30 13:58:35 +0300

    первый коммит

---
+++ src/main/java/pro/sky/hogw/HogwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.hogw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hogw/HogwApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hogw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
